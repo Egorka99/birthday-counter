30bde4be90aa71ff49df580a8f964d31ba2de68f
Egor <ze99@rambler.ru>
2020-11-15 14:00:48 +0300

    working project

--- BDcounter/app/src/main/java/com/tstu/MainActivity.java
+++ BDcounter/app/src/main/java/com/tstu/MainActivity.java
@@ -1,28 +1,24 @@
 package com.tstu;
 
-import android.net.Uri;
+import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
-import java.util.Locale;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     private final SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-    private EditText inputDate, textDaysLeft, textHoursLeft,text;
+    private EditText inputDate, textDaysLeft, textHoursLeft, text;
     private ConstraintLayout constraintLayout2;
     private static final int NOTIFY_ID = 101;
 
@@ -82,17 +78,15 @@ public class MainActivity extends AppCompatActivity {
             text.setText("До дня рождения осталось :");
             String date = inputDate.getText().toString();
             Utils.isValidDate(date);
-            date += "-"+LocalDate.now().getYear();
+            date += "-" + LocalDate.now().getYear();
             Date birthDayDate = Utils.getBirthdayDate(Objects.requireNonNull(format.parse(date)));
             long milliseconds = (birthDayDate.getTime() - new Date().getTime());
             long days = TimeUnit.MILLISECONDS.toDays(milliseconds);
             long hours = TimeUnit.MILLISECONDS.toHours(milliseconds) - TimeUnit.DAYS.toHours(days);
-            if(days != 364 ) {
+            if (days != 364) {
                 textDaysLeft.setText(String.valueOf(Math.abs(days)));
                 textHoursLeft.setText(String.valueOf(Math.abs(hours)));
-            }
-            else
-            {
+            } else {
                 text.setText("С днем рождения!");
                 constraintLayout2.setVisibility(View.INVISIBLE);
             }
